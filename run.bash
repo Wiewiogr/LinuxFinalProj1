025503cd4b301fa01f5840f05af077476f2fb53a
HomePC <wiewiora.tomasz1@gmail.com>
2016-12-31 12:31:08 +0100

    added echo in run.bash

--- run.bash
+++ run.bash
@@ -1,6 +1,9 @@
 rm -rf results generatorLogs powielaczLogs .secret fifos
 
 bash createFifos.bash 8
+echo "Generator logs in generatorLogs"
+echo "Powielacz logs in powielaczLogs"
+echo "Results in results"
 
 (./datownik.out -m0.5 | ./powielacz.out -c8 -pfifos/ -f120 -dpowielaczLogs) &
 (./generator.out -D0.1 -B13 -l10:20 -m0.5:1 -d0.2 -pfifos/ -c8 -Oresults -LgeneratorLogs)
--- test.bash
+++
@@ -1,1 +0,0 @@
-./generator.o -D0.1 -B13 -l10:20 -m0.5:1 -d0.2 -pfifos/ -c7 -Ooutput
